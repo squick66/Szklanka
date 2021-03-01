45d459068fa849822a676747a42d0193838fc329
uzytkownik <eset@gmail.com>
2021-03-01 23:06:02 +0100

    gitignore i pirwsze klasy

---
+++ src/main/java/Klasa/Klasa.java
@@ -0,0 +1,10 @@
+package Klasa;
+
+public class Klasa {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = (int) Math.pow(a,b);
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/Klasa/Klasa2.java
@@ -0,0 +1,4 @@
+package Klasa;
+
+public class Klasa2 {
+}
